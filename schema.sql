06d5e7ab9c81cffada17229ddd73e50923ad0a65
JR <j.romo15@outlook.com>
2018-10-15 20:10:14 -0700

    finished schema and seeds for mysql

--- schema.sql
+++ schema.sql
@@ -1,13 +1,14 @@
-DROP DATABASE IF EXISTS bamazon;
-CREATE DATABASE bamazon;
+DROP DATABASE IF EXISTS bamazon_db;
 
-USE bamazon;
+CREATE DATABASE bamazon_db;
 
-CREATE TABLE products (
-    item_id INT NOT NULL,
-    product_name VARCHAR (50),
-    department_name VARCHAR(50),
-    price DECIMAL(10,2),
-    stock_quantity INT(100)
+USE bamazon_db;
 
-)
+CREATE TABLE IF NOT EXISTS products (
+	item_id INT AUTO_INCREMENT,
+	product_name VARCHAR(255) NOT NULL,
+	department_name VARCHAR (255),
+    price DECIMAL(4,2),
+    stock_quantity TINYINT,
+    PRIMARY KEY(item_id)
+);
